5ef8eb12000d64ebba20426a875464194a1c7ab4
Sufyan Asghar <sufyanasghar231111@gmail.com>
2026-05-12 09:51:31 -0700

    Creating play music

--- frontend/spotify/src/component/home/AlbumDetail.jsx
+++ frontend/spotify/src/component/home/AlbumDetail.jsx
@@ -1,10 +1,13 @@
+import { RiPauseFill, RiPlayFill } from '@remixicon/react'
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const AlbumDetail = () => {
    let {id}=useParams()
+   let audioRef=useRef(null)
    let [detailData, setDetailData]=useState({})
+   let [playing, setPlaying]=useState(null)
    async function detail() {
     try{
       let res=await axios.get(`http://localhost:3000/api/creator/allAlbum/${id}`)
@@ -20,6 +23,26 @@ const AlbumDetail = () => {
   useEffect(()=>{
     detail()
   },[])
+
+  function playref(id){
+    let audio=audioRef.current
+    if(!audio) return null;
+    if(playing === id){
+      if(audio.paused){
+        audio.play()
+        setPlaying(true)
+      }else{
+        audio.pause()
+        setPlaying(false)
+      }
+    }
+    else{
+      let song=detailData.album?.find(elem => elem._id === id)
+      setPlaying(song._id)
+      audio.src=song.uri
+      audio.play()
+    }
+  }
 
   return (
        <div className='w-full max-sm:w-full ml-auto sticky rounded-lg overflow-hidden h-[76vh]'>
@@ -53,11 +76,14 @@ const AlbumDetail = () => {
               return  <div key={elem._id} className='flex  group hover:bg-white/10 transition-all duration-300  py-3 px-2 rounded-lg w-full  gap-6'>
             <div  className='relative'>
               <span className=' group-hover:scale-0  scale-100 absolute top-3 transition-all duration-300 '>{index+1}</span>
-               <span className=' group-hover:scale-100 scale-0  absolute top-3 transition-all duration-300'>p</span>
+               <span onClick={()=>{playref(elem._id)}} className=' group-hover:scale-100 scale-0  absolute top-3.5 -left-1 transition-all duration-300'>{
+                  playing === elem._id ? (<RiPauseFill className='text-white cursor-pointer w-5 h-5' />):(<RiPlayFill className='text-white cursor-pointer w-5 h-5' />)
+                }</span>
             </div>
             <div className='flex items-center  gap-3'>
-              <div className='w-12 h-12 border rounded'>
-                <audio src={elem.uri} />
+              <div className='w-12 h-12 border rounded overflow-hidden'>
+                <img className='w-full h-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9wl5xfVuzRmBNbqj8mKwsqAKwptQPO3LE7Q&s" alt="" />
+                <audio className='w-full ' ref={audioRef}  src={elem.uri} controls  />
               </div>
               <div>
                 <h1 className='font-semibold '>{detailData.artist?.username}</h1>
