7133a12ccb547b1197d4ac7d8a973a2ca128ea5c
Sufyan Asghar <sufyanasghar231111@gmail.com>
2026-05-28 02:15:41 -0700

    connecting all song with a unviersal song

--- frontend/spotify/src/component/home/Home.jsx
+++ frontend/spotify/src/component/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { RiChromeLine, RiHome4Fill, RiHome5Line, RiPlayFill, RiSearchLine, RiSpotifyFill } from '@remixicon/react'
+import { RiChromeLine, RiHome4Fill, RiHome5Line, RiPauseFill, RiPlayFill, RiSearchLine, RiSpotifyFill } from '@remixicon/react'
 import { Link, Outlet } from 'react-router-dom'
 import { authHome } from '../contextapi/HomeContext'
 import Logout from './Logout'
@@ -22,7 +22,6 @@ function Home() {
 
 
   let fetchname = (user.username.trim().split(' ')[0][0] + user.username.trim().split(' ').pop()[0]).toUpperCase()
-
 
 
   return (
@@ -89,25 +88,34 @@ function Home() {
         <Right />
 
       </div>
-      <div className='flex items-center justify-center pt-10'>
+      <div className='flex items-center justify-center '>
         {
           hideControl ? (
-            <div className='flex items-center gap-1'>
+            <div className='flex items-center gap-1 pt-8'>
               <h1 className='text-xs font-mono text-gray-400 w-12 text-right select-none'>00:00</h1>
               <input type="range" disabled className="cursor-not-allowed opacity-50 h-1.5 bg-gray-700 rounded-lg appearance-none " />
               <h1 className='text-xs font-mono text-gray-400 w-12 text-right select-none'>00:00</h1>
 
             </div>
           ) : (
-            <div className='flex items-center gap-2'>
+            <div className='flex items-center gap-25 pt-5 justify-between'>
               {
                 music.map((dataId) => {
                   return <>
                     {playing === dataId._id && (
                       <>
+                      <div className='-pt-10'>
+                        <h1 className='text-green-500 font-mono'>{dataId.title}</h1>
+                        <h1 className='text-sm text-[#807f7f] font-mono'>{dataId.artist?.username}</h1>
+                      </div>
+                      <div className='flex items-center justify-center gap-3'>
+                        {/* <div onClick={()=>{playRef(dataId._id)}} className='flex items-center justify-center gap-1'>
+                          {playing === dataId._id ? (<RiPauseFill  onClick={()=>{setHideControl(true)}} />):(<RiPlayFill onClick={()=>{setHideControl(true)}}  />)}
+                        </div> */}
                         <h1 className='text-xs font-mono text-gray-200 w-12 text-right select-none'>{Math.floor((currentTime[dataId._id] || 0) / 60)}: {String(Math.floor((currentTime[dataId._id] || 0)) % 60).padStart(2, '0')}</h1>
                         <input type="range" name="music" className='h-1.5 bg-gray-600 rounded-lg appearance-none cursor-pointer accent-green-500 hover:accent-green-400' onChange={(e) => { handleSeek(e, dataId._id) }} value={currentTime[dataId?._id] || 0} min='0' max={duration[dataId?._id] || 0} />
                         <h1 className='text-xs font-mono text-gray-200 w-12 text-right select-none'>{Math.floor((duration[dataId._id] || 0) / 60)}: {String(Math.floor((duration[dataId._id] || 0)) % 60).padStart(2, '0')}</h1>
+                      </div>
 
                       </>
                     )}
