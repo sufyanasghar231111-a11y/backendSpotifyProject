d3d5129d5511c76fcd5f2a8818f4f880dfb28211
Sufyan Asghar <sufyanasghar231111@gmail.com>
2026-05-27 22:30:57 -0700

    test lib and add link

--- frontend/spotify/src/component/home/2side/Left.jsx
+++ frontend/spotify/src/component/home/2side/Left.jsx
@@ -77,7 +77,7 @@ const Left = () => {
                         library.map((elem) => (
                            elem.music.map((item)=>{
                             return <div key={item._id} className=' flex items-center px-4 max-sm:px-2 py-2'>
-                                <Link >
+                                <Link to={`/detail/${item._id}`} >
                                     <div className='flex items-center gap-3'>
                                         <div className='flex items-center justify-center  overflow-hidden rounded w-13 max-sm:w-10 max-sm:h-10 h-13 bg-gradient-to-br from-[#3c17f5] via-[#8879ff] to-[#d7fff5] '>
                                             <img className='w-full h-full object-cover' src={item.image}  alt="" />
