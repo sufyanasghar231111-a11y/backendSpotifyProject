030d18f702569bb66cdfa0d89188d96d65acdb41
Sufyan Asghar <sufyanasghar231111@gmail.com>
2026-05-27 19:15:25 -0700

    create detail of album

--- frontend/spotify/src/component/home/Detail.jsx
+++ frontend/spotify/src/component/home/Detail.jsx
@@ -5,12 +5,12 @@ import Input from '../../like/Input'
 import Audioplay from '../play/Audioplay'
 // import { authProvider } from '../contextapi/AuthContext'
 const Detail = () => {
-   
-  
+
+
 
   return (
     <div className='text-white '>
-        
+
       <div className=' bg-[#1A1A1A] text-white flex items-center justify-between px-3 py-5'>
         <Audioplay />
       </div>
