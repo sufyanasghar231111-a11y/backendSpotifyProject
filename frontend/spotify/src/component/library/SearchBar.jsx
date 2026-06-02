ddd676b9b0f3a1bbb124942b2f1b91035772a731
Sufyan Asghar <sufyanasghar231111@gmail.com>
2026-06-02 00:42:15 -0700

    search problem in timming

--- frontend/spotify/src/component/library/SearchBar.jsx
+++ frontend/spotify/src/component/library/SearchBar.jsx
@@ -2,15 +2,16 @@ import React, { useContext } from 'react'
 import { authProvider } from '../contextapi/AuthContext'
 import { RiSearchLine } from '@remixicon/react'
 import { authHome } from '../contextapi/HomeContext'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function SearchBar() {
 
     let { hideSearch, setHideSearch } = useContext(authProvider)
-    let { searchMusic, music, Issearch, searchinput, loader,setSearchinput ,searchAlbum} = useContext(authHome)
+    let { searchMusic, music, Issearch, searchinput, loader, searchAlbum } = useContext(authHome)
 
-    let showsearch = searchinput.trim() ? [...searchMusic, ...searchAlbum] : music 
+    let showsearch = searchinput.trim() ? [...searchMusic, ...searchAlbum] : music
 
+    let navigate = useNavigate()
 
     return (
         <div>
@@ -25,7 +26,7 @@ function SearchBar() {
                                     <h1>Search</h1>
                                 </div>
                                 {
-                               searchinput.trim()==='' ? (
+                                    searchinput.trim() === '' ? (
                                         <div className='flex flex-col items-center justify-center py-14 text-center text-[#8a8a8a]'>
                                             <div className='w-16 h-16 rounded-full bg-[#1d1d1d] flex items-center justify-center text-2xl mb-4'>
                                                 <RiSearchLine />
@@ -42,18 +43,18 @@ function SearchBar() {
                                     ) : (
                                         <>
                                             {
-                                               searchinput.trim().length >1  && (
+                                                searchinput.trim().length > 1 && (
                                                     showsearch?.map((elem) => {
-                                                         
-                                                    return <Link key={elem._id} onClick={()=>{setHideSearch(false)
-                                                        setSearchinput('')
-                                                    }} to={`${elem.type==='music'? `/detail/${elem._id}`:`/albumdetail/${elem._id}`}`} >
-                                                     <div  className='mx-2 cursor-pointer hover:bg-[#404040] rounded-lg py-2  gap-6 px-3  flex items-center'>
-                                                        <h1 className='px-2.5 py-2.5 rounded-full bg-[#282828]'><RiSearchLine /></h1>
-                                                        <h1 className='font-semibold '>{elem.title}</h1>
-                                                    </div>
-                                                        </Link>
-                                                })
+
+                                                        return <div onClick={() => {
+                                                            navigate(`/searchmusic/${elem.title}`)
+                                                            setHideSearch(false)
+                                                        }} className='mx-2 cursor-pointer hover:bg-[#404040] rounded-lg py-2  gap-6 px-3  flex items-center'>
+                                                            <h1 className='px-2.5 py-2.5 rounded-full bg-[#282828]'><RiSearchLine /></h1>
+                                                            <h1 className='font-semibold '>{elem.title}</h1>
+                                                        </div>
+
+                                                    })
                                                 )
                                             }
                                         </>
@@ -61,7 +62,7 @@ function SearchBar() {
                                 }
 
                                 {
-                                    loader && searchMusic.length===0 && searchAlbum.length===0  && searchinput.trim().length<2 && (
+                                    loader && searchMusic.length === 0 && searchAlbum.length === 0 && searchinput.trim().length < 2 && (
                                         <div className=' absolute w-100  h-90 z-150 bg-black inset-0'>
                                             <div className='flex flex-col justify-center w-full h-full items-center gap-4'>
                                                 <div className='w-10 h-10 border-[3px] border-[#1DB954] border-t-transparent rounded-full animate-spin'></div>
@@ -75,7 +76,7 @@ function SearchBar() {
                                 }
                                 {
                                     !loader &&
-                                    searchinput.trim() !== '' && searchMusic.length === 0 && searchAlbum.length===0 && (
+                                    searchinput.trim() !== '' && searchMusic.length === 0 && searchAlbum.length === 0 && (
                                         <div> <div className='flex flex-col items-center justify-center py-14 text-center text-[#8a8a8a]'>
 
                                             {/* Icon */}
